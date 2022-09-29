59d19fcb08c6d3be1d5227d29cc445a038beaeee
Will Bonner <89400747+spookyscaryyy@users.noreply.github.com>
2022-09-29 03:16:56 -0400

    Updated tabulation
    fixed some weird tab characters

--- proj-1.asm
+++ proj-1.asm
@@ -31,30 +31,30 @@ main:
 
 	bne		$zero,	$s1,	inittest		# enter loop
 	initloop:
-		sw 		$t1,	0($t2)				# grab array[i]
+		sw 	$t1,	0($t2)				# grab array[i]
 		addi 	$t2,	$t2,	4			# prepare for indexing - next array element
 		addi 	$t1,	$t1,	1			# # i++;
 	inittest:
 		slti	$t0,	$t1,	101			# i <= 100
-		bne 	$t0,	$zero,	initloop	# while (i <= 100)
+		bne 	$t0,	$zero,	initloop		# while (i <= 100)
 	
 		bne		$zero,	$s1,	maintest	# enter loop
 	mainloop:
 		# body of main while loop
 
 		# start of sieve
-			sll		$t1,	$s1,	1			# i = prime * 2
-			bne		$zero,	$s1,	sievetest	# enter loop
+			sll	$t1,	$s1,	1		# i = prime * 2
+			bne	$zero,	$s1,	sievetest	# enter loop
 		sieveloop:
 			# body of sieve while loop
 			addi	$t2,	$t1,	-1			# i - 1
 			sll 	$t2,	$t2,	2			# prepare for indexing
-			add		$t2,	$s0,	$t2			# array[i-1]
-			sw		$zero,	0($t2) 				# array[i-1] = 0
-			add		$t1,	$t1,	$s1			# i = i + prime
+			add	$t2,	$s0,	$t2			# array[i-1]
+			sw	$zero,	0($t2) 				# array[i-1] = 0
+			add	$t1,	$t1,	$s1			# i = i + prime
 		sievetest:
 			slti	$t0,	$t1,	101			# i <= 100
-			bne		$t0,	$zero,	sieveloop	# while (i <= 100)
+			bne	$t0,	$zero,	sieveloop		# while (i <= 100)
 		# end of sieve
 
 
@@ -62,17 +62,17 @@ main:
 			addi	$t1,	$s1,	0			# i = 0 + prime
 			addi	$t2,	$t1,	0			# copy iterator
 			sll 	$t2,	$t2,	2			# prepare for indexing
-			add		$t2,	$s0,	$t2			# array[i] index
-			bne 	$zero,	$s1,	primetest	# enter loop
+			add	$t2,	$s0,	$t2			# array[i] index
+			bne 	$zero,	$s1,	primetest		# enter loop
 		primeloop:
 			addi	$t1,	$t1,	1			# i++;
 			addi	$t2,	$t2,	4			# grab next array index
 		primetest:
-			lw 		$t3,	0($t2)				# grab array[i]
+			lw 	$t3,	0($t2)				# grab array[i]
 			slti	$t0,	$t1,	100			# i < 100
 			slti	$t4,	$t3,	1			# array[i] < 1
-			and		$t0,	$t0,	$t4			# array[i] == 0 && i < 100
-			bne		$t0,	$zero,	primeloop	# while (array[i] == 0 && i < 100)
+			and	$t0,	$t0,	$t4			# array[i] == 0 && i < 100
+			bne	$t0,	$zero,	primeloop		# while (array[i] == 0 && i < 100)
 
 			addi	$s1,	$t1,	1			# prime = i + 1;
 		# end of prime finding
@@ -80,7 +80,7 @@ main:
 
 	maintest:
 		slti	$t0,	$s1,	100			# prime < 100
-		bne		$t0,	$zero,	mainloop	# while (prime < 100)
+		bne	$t0,	$zero,	mainloop		# while (prime < 100)
 	# end main
 
 	li $v0, 10	# exit program
