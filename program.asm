8f018f9b8526c6b1aa6a4344653dc327944f1618
Will Bonner <89400747+spookyscaryyy@users.noreply.github.com>
2022-10-23 21:43:17 -0400

    Finished code
    converted to version for modelsim
    converted code to hex

--- program.asm
+++ program.asm
@@ -11,43 +11,13 @@
 # t6 = rbit
 # t7 = function result and conditional
 
-# s0 = pal_count
-# s1 = word
-# s2 = break cond for while loop
-# s3 = conditional
-
 # IDEALLY only use these insts: 
 # sll (only by 1), srl (only by 1), sub, subi, slt, or, ori, 
 # beq,  bne,  add,  addi,  and,  andi,  lui
 
 main: 
-    add  $s0, $0,  $0
-    lui  $s1, 0x000F
-    ori  $s1, $s1, 0x4240 
-    addi $s2, $s2, 6        # load constants
-    beq  $0,  $0,  mainWhileTest
-    mainWhileBody:
-        or   $a0, $s1, $0
-        jal  isBinPal
-        beq  $v0, $0, notPal
-        or   $a0, $s1, $0
-        ori  $v0, $0,  1
-        syscall             # print word
-        ori  $a0, $0,  10
-        ori  $v0, $0,  11
-        syscall             # print new line character
-        addi $s0, $s0, 1
-    notPal:
-        addi $s1, $s1, 1
-    mainWhileTest:
-        slt  $s3, $s0, $s2
-        bne  $s3, $0,  mainWhileBody
-
-    ori  $v0, $0,  10 
-    syscall 
- 
-isBinPal: 
-    or   $t0, $0, $a0   # load word into t0
+    lui  $t0, 0x000F
+    ori  $t0, $t0, 0x462F 
     beq  $t0, $0, False # if (word == 0) {return false}
     lui  $t1, 0x8000    # load constants
     addi $t2, $0, 1
@@ -84,8 +54,11 @@ isBinPal:
         bne  $t7, $0, forBody
     
     # Return section
-    addi $v0, $0, 1
-    jr   $ra            # return true
+    addi $t7, $0,  1
+    beq  $t7, $t7, done            # return true
     False:
-        addi $v0, $0, 0
-        jr   $ra        # return false
+        addi  $t7, $0,  0
+        beq   $t7, $t7, done        # return false
+
+    done:
+        beq  $t7, $t7, done
